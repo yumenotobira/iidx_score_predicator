a29aac6869146bf861e258116aa28dacff9e755b
yumenotobira <grnggm+yumenotobira@gmail.com>
2020-11-06 08:50:08 +0900

    Fix NN

--- predicator/train_beat.py
+++ predicator/train_beat.py
@@ -11,53 +11,8 @@ from keras import backend as K
 import os
 import pickle
 
-def norm_kl_divergence(mean_true, mean_pred, sd_true, sd_pred):
-    return K.log(sd_pred / sd_true) + (K.square(sd_true) + K.square(mean_true - mean_pred)) / (2.0 * K.square(sd_pred)) - 1.0 / 2.0
-
-def custom_loss(y_true, y_pred):
-    weight_true = y_true[:, 0:5]
-    weight_pred = y_pred[:, 0:5]
-    mean_true = y_true[:, 5:10]
-    mean_pred = y_pred[:, 5:10]
-    sd_true = y_true[:, 10:15]
-    sd_pred = y_pred[:, 10:15]
-
-    sum_a = 0
-    for i in range(5):
-        weight_a = weight_true[:, i]
-        mean_a = mean_true[:, i]
-        sd_a = sd_true[:, i]
-
-        sum_b = 0
-        sum_c = 0
-        for j in range(5):
-            weight_b = weight_true[:, j]
-            mean_b = mean_true[:, j]
-            sd_b = sd_true[:, j]
-            weight_c = weight_pred[:, j]
-            mean_c = mean_pred[:, j]
-            sd_c = sd_pred[:, j]
-
-            kl_ab = norm_kl_divergence(mean_a, mean_b, sd_a, sd_b)
-            kl_ac = norm_kl_divergence(mean_a, mean_c, sd_a, sd_c)
-
-            if j == 0:
-              sum_b = weight_b * K.exp(-kl_ab)
-              sum_c = weight_c * K.exp(-kl_ac)
-            else:
-              sum_b = sum_b + weight_b * K.exp(-kl_ab)
-              sum_c = sum_c + weight_c * K.exp(-kl_ac)
-
-        if i == 0:
-          sum_a = weight_a * K.log(sum_b/sum_c)
-        else:
-          sum_a = sum_a + weight_a * K.log(sum_b/sum_c)
-
-    loss = sum_a
-    return loss
-
 # 取得するファイル名一覧
-posixPath = list(pathlib.Path('/content/drive/My Drive/beatmania/chart_encoded').glob('*'))
+posixPath = list(pathlib.Path('/content/drive/My Drive/beatmania/chart_encoded_rename').glob('*'))
 fileNames = list(map(lambda x: x.name, posixPath))
 
 # 譜面データと分布パラメーターの取得
@@ -69,8 +24,9 @@ for fileName in fileNames:
     print(str(index) + ": " + fileName)
     index += 1
     for number in range(1):
+      fileNameWithNumber = fileName + "_" + "{:02d}".format(number)
       # 譜面データの取得
-      chart = np.loadtxt('/content/drive/My Drive/beatmania/chart_encoded/'+ fileName, delimiter=',')
+      chart = np.loadtxt('/content/drive/My Drive/beatmania/rotate_chart_encoded/'+ fileNameWithNumber, delimiter=',')
 
       # BPMを1/100倍する
       chart[:, -1] /= 100
@@ -88,13 +44,16 @@ for fileName in fileNames:
       charts.append(splitted_chart)
 
       # パラメーターの取得
-      f = open('/content/drive/My Drive/beatmania/gmm_result/' + fileName)
+      f = open('/content/drive/My Drive/beatmania/gmm_result_rename/' + fileName)
       param = f.read()
       f.close()
 
       param = param.replace('\n', ',')
       param = param.split(',')
       param.pop(-1)
+      # sdを100倍してオーダーを上げる
+      param = list(map(lambda x: float(x), param))
+      param[10:15] = list(map(lambda x: x*100, param[10:15]))
 
       l = list()
       for t in param:
@@ -121,31 +80,43 @@ model_input = Input(shape=(None, 9*96))
 mask = Masking(mask_value=-1.0)(model_input)
 
 weight_decay = 1e-4
-mid1 = LSTM(units=128, dropout=0.25, return_sequences=True)(mask)
-mid1 = LSTM(units=128, dropout=0.25, return_sequences=True)(mid1)
-mid2 = LSTM(units=128, dropout=0.25, return_sequences=True)(mid1)
-mid2 = LSTM(units=128, dropout=0.25, return_sequences=True)(mid2)
-mid2 = LSTM(units=128, dropout=0.25, return_sequences=True)(mid2)
-mid3 = LSTM(units=128, dropout=0.25, return_sequences=False,
-        kernel_regularizer=regularizers.l2(weight_decay))(mid2)
 
-additional_dense1 = Dense(5)(mid3)
+mid1w = LSTM(units=128, dropout=0.25, return_sequences=True)(mask)
+mid2w = LSTM(units=128, dropout=0.25, return_sequences=True)(mid1w)
+mid3w = LSTM(units=128, dropout=0.25, return_sequences=False,
+        kernel_regularizer=regularizers.l2(weight_decay))(mid2w)
+mid1m = LSTM(units=128, dropout=0.25, return_sequences=True)(mask)
+mid2m = LSTM(units=128, dropout=0.25, return_sequences=True)(mid1m)
+mid3m = LSTM(units=128, dropout=0.25, return_sequences=False,
+        kernel_regularizer=regularizers.l2(weight_decay))(mid2m)
+mid1s = LSTM(units=128, dropout=0.25, return_sequences=True)(mask)
+mid2s = LSTM(units=128, dropout=0.25, return_sequences=True)(mid1s)
+mid3s = LSTM(units=128, dropout=0.25, return_sequences=False,
+        kernel_regularizer=regularizers.l2(weight_decay))(mid2s)
+
+for_w = Dense(128, activation='relu')(mid3w)
+for_w = Dropout(0.25)(for_w)
+for_m = Dense(128, activation='relu')(mid3m)
+for_m = Dropout(0.25)(for_m)
+for_s = Dense(128, activation='relu')(mid3s)
+for_s = Dropout(0.25)(for_s)
+
+additional_dense1 = Dense(5)(for_w)
 output1 = Activation('softmax')(additional_dense1)
-additional_dense2 = Dense(10)(mid3)
+additional_dense2 = Dense(5)(for_m)
 output2 = Activation('sigmoid')(additional_dense2)
-merged = concatenate([output1, output2])
+additional_dense3 = Dense(5)(for_s)
+output3 = Activation('relu')(additional_dense3)
+
+merged = concatenate([output1, output2, output3])
 model = Model(inputs=[model_input], outputs=[merged])
+model.compile(loss='mean_absolute_error', optimizer=optimizers.Adam(), metrics=['accuracy'])
 
-model.compile(loss=custom_loss, optimizer=optimizers.Adam(), metrics=['accuracy'])
+fpath = os.path.join('/content/drive/My Drive/beatmania/model/', 'model_weights.{epoch:05d}-{val_loss:.2f}.hdf5')
+cp_cb = callbacks.ModelCheckpoint(filepath=fpath, monitor='val_loss', verbose=0, save_weights_only = True, save_best_only=True, mode='auto')
+cp_es = callbacks.EarlyStopping(monitor='val_loss', patience=100, verbose=1, mode='auto')
 
-# 重みの読み込み
-# 以前の重みを使って学習するときはコメントアウトを外す
-#model.load_weights(os.path.join('/content/drive/My Drive/beatmania/model/', 'model_weights.hdf5'))
-
-fpath = os.path.join('/content/drive/My Drive/beatmania/model/', 'model_weights.{epoch:04d}-{val_loss:.2f}.hdf5')
-cp_cb = callbacks.ModelCheckpoint(filepath=fpath, monitor='val_loss', verbose=1, save_weights_only = True, save_best_only=True, mode='auto')
-
-history = model.fit(charts, params, batch_size=32, verbose=0, initial_epoch=0, epochs=5000, validation_split=0.1, callbacks=[cp_cb])
+history = model.fit(charts, params, batch_size=32, verbose=2, initial_epoch=0, epochs=1000, validation_split=0.1, callbacks=[cp_cb, cp_es])
 
 # 保存
 json_string = model.to_json()
